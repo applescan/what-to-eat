ea403dfd1aa6d0ad366aefca8a3d7e734cb7d222
applescan <feliu.ren@gmail.com>
2023-03-29 20:47:37 +1300

    Added new page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function Document() {
   return (
@@ -9,8 +10,10 @@ export default function Document() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <body>
+        <Header></Header>
         <Main />
         <NextScript />
+        <Footer></Footer>
       </body>
     </Html>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import Header from "@/components/Header"
 import HeroBanner from "@/components/Home/HeroBanner";
-import Footer from "@/components/Footer";
 import Diets from "@/components/Home/Diets";
 
 export default function Index(): JSX.Element {
 
   return (
     <>
-      <Header></Header>
       <HeroBanner></HeroBanner>
       <Diets></Diets>
-      <Footer></Footer>
     </>
   )
 }
