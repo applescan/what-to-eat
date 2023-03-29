3128103158efdc757c4a5303ab5df187ea382ff9
applescan <feliu.ren@gmail.com>
2023-03-29 20:18:50 +1300

    Finished homepage styling

--- components/Home/Diets.tsx
+++ components/Home/Diets.tsx
@@ -1,95 +1,67 @@
+import Image from "next/image";
+import Diet from "../../public/dietary.png"
+import Kitchen from "../../public/kitchen.png"
+import Food from "../../public/food.png"
+
 export default function Diets(): JSX.Element {
 
-    interface FeaturesList {
-        icon: any,
-        title: string,
-        desc: string
+    interface StepsItem {
+        title: string;
+        desc: string;
+        src: any;
+        alt: string;
     }
 
-    const features: FeaturesList[] = [
+    const steps: StepsItem[] = [
         {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
-                </svg>,
-            title: "Analytics",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius."
+            title: "Choose your dietary preferences",
+            desc: "We have many dietary options to suit your needs!",
+            src: Diet,
+            alt: "Dietary icon"
+
+        }, {
+            title: "Tell us what you have in your kitchen",
+            desc: "Calamary? Lettuce? Banana? Nothing is off limit",
+            src: Kitchen,
+            alt: "Kitchen icon"
+
+        }, {
+            title: "Get reccomendation",
+            desc: "No more browsing mindlessly 🔥",
+            src: Food,
+            alt: "Food icon"
+
         },
-        {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                </svg>,
-            title: "Datacenter security",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius."
-        },
-        {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
-                </svg>,
-            title: "Build on your terms",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius."
-        },
-        {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
-                </svg>,
-            title: "Analytics",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius."
-        },
-        {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                </svg>,
-            title: "Datacenter security",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius."
-        },
-        {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
-                </svg>,
-            title: "Build on your terms",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius."
-        }
     ]
 
     return (
-        <section className="py-14">
-            <div className="max-w-screen-xl mx-auto px-10 text-gray-600 md:px-8">
-                <div className="max-w-xl space-y-3">
-                    <h3 className="text-indigo-600 font-semibold">
-                        Features
-                    </h3>
-                    <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                        Do more with less complexity
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius
-                    </p>
+        < section className="py-16" >
+            <div className="max-w-screen-xl mx-auto px-10 md:px-8">
+                <div className="max-w-md">
+                    <h1 className="text-gray-700 text-xl font-extrabold sm:text-2xl">Easy Steps</h1>
+                    <p className="text-gray-600 mt-2">Get reccomendations in 3 easy steps</p>
                 </div>
-                <div className="mt-12">
-                    <ul className="grid gap-x-12 divide-y [&>.feature-1]:pl-0 sm:grid-cols-2 sm:gap-y-8 sm:divide-y-0 lg:divide-x lg:grid-cols-3 lg:gap-x-0">
-                        {
-                            features.map((item, idx) => (
-                                <li key={idx} className={`feature-${idx + 1} space-y-3 py-8 lg:px-12 sm:py-0`}>
-                                    <div className="w-12 h-12 border text-indigo-600 rounded-full flex items-center justify-center">
-                                        {item.icon}
+                <ul className="mt-16 grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                    {
+                        steps.map((item, idx) => (
+                            <li key={idx} className="border rounded-lg">
+                                <div className="flex items-start justify-between p-4">
+                                    <div className="space-y-2">
+                                        <Image
+                                            src={item.src}
+                                            width={100}
+                                            height={50}
+                                            alt={item.alt}
+                                        />
+                                        <h4 className="text-gray-800 font-semibold">{item.title}</h4>
+                                        <p className="text-gray-600 text-sm">{item.desc}</p>
                                     </div>
-                                    <h4 className="text-lg text-gray-800 font-semibold">
-                                        {item.title}
-                                    </h4>
-                                    <p>
-                                        {item.desc}
-                                    </p>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
+                                </div>
+
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </section>
     )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>What to eat</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
