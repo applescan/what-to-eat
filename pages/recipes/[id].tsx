47b0f73201a390e8cdcc95e3db4c0b7b90bc2609
Felicia <107516611+applescan@users.noreply.github.com>
2023-04-07 23:07:48 +1200

    Update [id].tsx

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -173,7 +173,7 @@ const RecipePage = ({ recipe }: RecipePageProps) => {
                             </>
                         )}
                     </div>
-                    <div className='mt-12'>
+                    <div className='mt-12 w-1/2'>
                         <Link className="mx-auto flex justify-center gap-x-2 py-2 px-10 w-full text-sm text-white font-medium bg-teal-400 hover:bg-teal-500 active:bg-teal-600 duration-150 rounded-lg sm:mt-0 sm:w-1/4" href={{ pathname: "/recipes" }}>
                             Back </Link>
                     </div>
